a8893a45dff29b40b11123e50376c31f2294fb18
RufaidaMamun <rufaidamamun@gmail.com>
2023-03-20 23:42:11 +0600

    Lab 5.2

---
+++ Lab_5.2/PublicHome.php
@@ -0,0 +1,27 @@
+<?php 
+    session_start();
+   // print_r($_SESSION);
+?>
+
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <h1>Welcome to Home Page </h1>
+    <body>
+    <table border="1" width="500px" align="center">
+        <tr>
+            <td colspan="2"> <img src="x logo.png" width="50px" height="50px" aling="center" />
+            <br> 
+            <a href="Plogin.php">Login </a> 
+            <br>
+            <a href="Register.php">Registration</a>
+        
+        </tr>
+
+        <tr>
+           <h4> <td colspan="2" aling="center"width="200" height="200">Welcome To Home Page!</td></h4>
+        
+
+    </body>
+</html>
