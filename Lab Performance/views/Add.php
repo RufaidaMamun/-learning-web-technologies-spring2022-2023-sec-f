299bee97745fc9124f2b2a36a0f3a10bc157dea0
RufaidaMamun <103326399+RufaidaMamun@users.noreply.github.com>
2023-04-06 14:26:17 +0600

    Add files via upload

---
+++ Lab Performance/views/Add.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+   <body>
+
+     
+     
+            <form method="post" action="abc.html" enctype="">
+            <fieldset>
+                <legend>Add product</legend>
+            Name: <br> <input type="text" name="name" value=""/> <br>
+            Buying price :<br> <input type="number" name="bprice" value=""/> <br/>
+            Selling price :<br><input type="number" name="sprice" value=""/> <br/>
+            <input type="submit" name="submit" value="submit"/>
+            
+        </fieldset>
+        </form>      
+    
+   </body>
